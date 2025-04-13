62fb16f783a0258f467ff3ebd4a42ccdab615c25
Kenny Lee <160ken.qq@gmail.com>
2025-04-13 19:50:23 +0100

    This class is completed

--- FitnessTracker.java
+++ FitnessTracker.java
@@ -2,6 +2,6 @@ import javax.swing.*;
 
 public class FitnessTracker {
   public static void main(String[] args) {
-    SwingUtilities.invokeLater(FitnessGUI::new);
+    SwingUtilities.invokeLater(Login::new);
   }
 }
