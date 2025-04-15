7bdd9e4ecbfc6be82cf3415568dfe05b79fa70bb
Kenny Lee <160ken.qq@gmail.com>
2025-04-15 22:47:57 +0100

    This class is completed
    Added comments to better understand the code

--- FitnessTracker.java
+++ FitnessTracker.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
-
+/*This class chooses which class to run first as each
+ * class has a window in the code*/
 public class FitnessTracker {
   public static void main(String[] args) {
     SwingUtilities.invokeLater(Login::new);
