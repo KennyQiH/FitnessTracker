c5fec64140bd3ff8e8b60f24b5ad2b1006af1c09
Kenny Lee <160ken.qq@gmail.com>
2025-04-20 18:25:25 +0100

    Update Workout.java

--- Workout.java
+++ Workout.java
@@ -3,10 +3,12 @@ import javax.swing.*;
 import java.util.ArrayList;
 
 public class Workout extends JFrame {
+	//Creates the variables
 	private JTextField Type, Duration, Calories;
 	private JTextArea Logs;
 	private static ArrayList<String> Workouts = new ArrayList<>();
 
+	//Layout of the window
 	public Workout() {
 	  setTitle("Workout Log");
 	  setSize(400,300);
@@ -14,15 +16,15 @@ public class Workout extends JFrame {
 	  setLocationRelativeTo(null);
     
 	  add(new JLabel("Exercise:"));
-	  Type = new JTextField();
+	  Type = new JTextField("Bench Press");
 	  add(Type);
     
-	  add(new JLabel("Duration:"));
-	  Duration = new JTextField();
+	  add(new JLabel("Duration (in minutes):"));
+	  Duration = new JTextField("30");
 	  add(Duration);
 	  
 	  add(new JLabel("Calories Burned:"));
-	  Calories = new JTextField();
+	  Calories = new JTextField("100");
 	  add(Calories);
     
 	  JButton logButton = new JButton("Log workout");
@@ -35,13 +37,28 @@ public class Workout extends JFrame {
 	  
 	  setVisible(true);
   }
+	//The code for this window
   	private void LogWorkout() {
+  		//creating a negative response for invalid inputs
   		try {
   			String type = Type.getText().trim();
-  			int duration = Integer.parseInt(Duration.getText().trim());
-  			int calories = Integer.parseInt(Calories.getText().trim());
+  			String durationText = Duration.getText().trim();
+  			String caloriesText = Calories.getText().trim();
   			
-  			String Workout = "Workout: " + Type.getText() + ", Duration: " + Duration.getText() + "minutes Calories Burned: " + Calories.getText();
+  			if (type.isEmpty() || durationText.isEmpty() || caloriesText.isEmpty()) {
+  				JOptionPane.showMessageDialog(this, "All fields must be filled out");
+  				return;
+  			}
+  			//assigning integer values so it can check whether if it is a number
+  			int duration = Integer.parseInt(durationText);
+  			int calories = Integer.parseInt(caloriesText);
+  			
+  			if (duration < 0 || calories < 0) {
+  				JOptionPane.showMessageDialog(this, "Must be a positive value");
+  				return;
+  			}
+  			//Format of the response
+  			String Workout = "Workout: " + type + ", Duration: " + duration + "minutes Calories Burned: " + calories;
   			Workouts.add(Workout);
   			Logs.setText(String.join("\n", Workouts));
   		} catch (NumberFormatException ex){
